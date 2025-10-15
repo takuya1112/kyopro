960b82236624930fba2b7caf854610937f770547
Takuya Muraoka <muraoka1112@icloud.com>
2025-10-15 14:10:36 +0900

    test

---
+++ solve_later/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define rrep(i,a,b) for(int i = a; i >= b; i--)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define chmin(x, y) x = min(x, y)
+#define chmax(x, y) x = max(x, y)
+typedef long long ll; const int inf = INT_MAX / 2;
+
+int main() {
+    return 0;
+}
