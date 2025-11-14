ec53d9398d380e59a60706fec6a2d5b920ef2ac1
Takuya Muraoka <muraoka1112@icloud.com>
2025-11-14 10:07:37 +0900

    change

---
+++ atcoder/PAST_beginner/dp/dp_d/dp_d.cpp
@@ -0,0 +1,153 @@
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+// --- utility ---
+template<class S, class T> inline bool chmax(S &a, T b) { return (a < b ? a = b, 1 : 0); }
+template<class S, class T> inline bool chmin(S &a, T b) { return (a > b ? a = b, 1 : 0); }
+
+using pint = pair<int, int>;
+using pll = pair<long long, long long>;
+using tint = array<int, 3>;
+using tll = array<long long, 3>;
+using fint = array<int, 4>;
+using fll = array<long long, 4>;
+using qint = array<int, 5>;
+using qll = array<long long, 5>;
+using vint = vector<int>;
+using vll = vector<long long>;
+using vs = vector<string>;
+using ll = long long;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+template <class T> using pq = priority_queue<T, vector<T>>;
+template <class T> using pq_g = priority_queue<T, vector<T>, greater<T>>;
+
+// --- modular ---
+// using mint = modint998244353;
+// using mint = modint1000000007;
+
+// --- macros ---
+#define rep(i, a) for (long long i = 0; i < (long long)(a); i++)
+#define rep2(i, a, b) for (long long i = a; i < (long long)(b); i++)
+#define rrep(i, a) for (long long i = (a)-1; i >= (long long)(0); --i)
+#define rrep2(i, a, b) for (long long i = (b)-1; i >= (long long)(a); --i)
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define next_p(x) next_permutation(x.begin(), x.end())
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#ifndef ONLINE_JUDGE
+// --- debug macros ---
+#define debug(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+// --- debug stream ---
+template<class T1, class T2> ostream& operator << (ostream &s, pair<T1,T2> P)
+{ return s << '<' << P.first << ", " << P.second << '>'; }
+template<class T> ostream& operator << (ostream &s, array<T, 3> P)
+{ return s << '<' << P[0] << ", " << P[1] << ", " << P[2] << '>'; }
+template<class T> ostream& operator << (ostream &s, array<T, 4> P)
+{ return s << '<' << P[0] << ", " << P[1] << ", " << P[2] << ", " << P[3] << '>'; }
+template<class T> ostream& operator << (ostream &s, vector<T> P)
+{ for (int i = 0; i < P.size(); ++i) { if (i > 0) { s << " "; } s << P[i]; } return s; }
+template<class T> ostream& operator << (ostream &s, deque<T> P)
+{ for (int i = 0; i < P.size(); ++i) { if (i > 0) { s << " "; } s << P[i]; } return s; }
+template<class T> ostream& operator << (ostream &s, vector<vector<T> > P)
+{ for (int i = 0; i < P.size(); ++i) { s << endl << P[i]; } return s << endl; }
+template<class T> ostream& operator << (ostream &s, set<T> P)
+{ for (auto it : P) { s << "<" << it << "> "; } return s; }
+template<class T> ostream& operator << (ostream &s, multiset<T> P)
+{ for (auto it : P) { s << "<" << it << "> "; } return s; }
+template<class T> ostream& operator << (ostream &s, unordered_set<T> P)
+{ for (auto it : P) { s << "<" << it << "> "; } return s; }
+template<class T1, class T2> ostream& operator << (ostream &s, map<T1,T2> P)
+{ for (auto it : P) { s << "<" << it.first << "->" << it.second << "> "; } return s; }
+template<class T1, class T2> ostream& operator << (ostream &s, unordered_map<T1,T2> P)
+{ for (auto it : P) { s << "<" << it.first << "->" << it.second << "> "; } return s; }
+#endif // ONLINE_JUDGE
+
+// --- directions ---
+const vector<int> dx = {1, 0, -1, 0};
+const vector<int> dy = {0, 1, 0, -1};
+const vector<int> dx8 = {1, 0, -1, 0, 1, -1, 1, -1};
+const vector<int> dy8 = {0, 1, 0, -1, 1, 1, -1, -1};
+
+// --- bit operations ---
+int popcnt(int x) { return __builtin_popcount(x); }
+int popcnt(unsigned int x) { return __builtin_popcount(x); }
+int popcnt(long long x) { return __builtin_popcountll(x); }
+int popcnt(unsigned long long x) { return __builtin_popcountll(x); }
+
+int bsf(int x) { return __builtin_ctz(x); }
+int bsf(unsigned int x) { return __builtin_ctz(x); }
+int bsf(long long x) { return __builtin_ctzll(x); }
+int bsf(unsigned long long x) { return __builtin_ctzll(x); }
+
+// --- math utils ---
+template<class T> T floor(T a, T b) {
+if (a % b == 0 || a >= 0) return a / b;
+else return -((-a) / b) - 1;
+}
+template<class T> T ceil(T x, T y) {
+return floor(x + y - 1, y);
+}
+// --- constants ---
+
+const int INF = 1e9;
+const ll LINF = 4e18;
+
+// ========================================
+//                  solve
+// ========================================
+
+int N, W, w[101], v[101];
+ll dp[101][201010];
+
+int main() {
+    cin >> N >> W;
+    rep(i, N) cin >> w[i] >> v[i];
+
+    rep(i, N) rep(tot, W) {
+        chmax(dp[i + 1][tot], dp[i][tot]);
+        chmax(dp[i + 1][tot + w[i]], dp[i][tot] + v[i]);
+    }
+
+    ll ans = 0;
+    rep(tot, W + 1) chmax(ans, dp[N][tot]);
+    cout << ans << endl;
+    return 0;
+}
+
+// mycode AC
+// int main() {
+//     int N, W;
+//     cin >> N >> W;
+//     vint w(N), v(N);
+//     rep(i, N) cin >> w[i] >> v[i];
+
+//     vll dp(W+1, -LINF);
+//     dp[0] = 0;
+//     rep(i, N) {
+//         vll nxt(W+1, -LINF);
+//         rep(j, W+1) {
+//             chmax(nxt[j], dp[j]);
+//             if (j >= w[i]) chmax(nxt[j], dp[j - w[i]] + v[i]);
+//         }
+//         swap(dp, nxt);
+//     }
+//     ll ans = 0;
+//     rep(i, W+1) chmax(ans, dp[i]);
+//     cout << ans << endl; 
+//     return 0;
+// }
