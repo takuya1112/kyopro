cef3a8819dba9db4217621563f7db65b08a367db
Takuya Muraoka <muraoka1112@icloud.com>
2025-12-06 18:38:23 +0900

    change

--- library/kypro_tool/add_snip/function_file/crud/read/list.hpp
+++ library/kypro_tool/add_snip/function_file/crud/read/list.hpp
@@ -8,46 +8,15 @@ using json = nlohmann::json;
 using namespace std;
 
 void list_name(const json& data, bool full = false) {
-    vector<pair<string, json>> new_data = filter_sort(data, filter_all, sorter_name);
-    
-    cout << "=====登録スニペット一覧=====\n";
-    for (auto& [name, snip] : new_data) {
-        cout << "name: " << name << "\n";
-        cout << "prefix: " << snip["prefix"].get<string>() << "\n";
-        if (full) {
-            if (snip.contains("description") && !snip["description"].empty()) {
-                cout << "description: " << snip["description"].get<string>() << "\n";
-            } 
-
-            if (snip.contains("tags") && !snip["tags"].empty()) {
-                cout << "tags: ";
-                for (auto& t : snip["tags"].get<vector<string>>()) cout << t << " ";
-                cout << "\n";
-            }
-        }
-        cout << "----------------------------\n";
-    }
+    Filter_Sorter fs;
+    fs.filter = filter_all;
+    fs.sorter = sorter_name;
+    fs.print_data(data, full);
 }
 
-
 void list_prefix(const json& data, bool full = false) {
-    vector<pair<string, json>> new_data = filter_sort(data, filter_all, sorter_prefix);
-    
-    cout << "=====登録スニペット一覧=====\n";
-    for (auto& [name, snip] : new_data) {
-        cout << "name: " << name << "\n";
-        cout << "prefix: " << snip["prefix"].get<string>() << "\n";
-        if (full) {
-            if (snip.contains("description") && !snip["description"].empty()) {
-                cout << "description: " << snip["description"].get<string>() << "\n";
-            } 
-
-            if (snip.contains("tags") && !snip["tags"].empty()) {
-                cout << "tags: ";
-                for (auto& t : snip["tags"].get<vector<string>>()) cout << t << " ";
-                cout << "\n";
-            }
-        }
-        cout << "----------------------------\n";
-    }
+    Filter_Sorter fs;
+    fs.filter = filter_all;
+    fs.sorter = sorter_prefix;
+    fs.print_data(data, full);
 }
--- library/kypro_tool/add_snip/function_file/crud/read/master.hpp
+++ library/kypro_tool/add_snip/function_file/crud/read/master.hpp
@@ -7,19 +7,44 @@
 using json = nlohmann::json;
 using namespace std;
 
-vector<pair<string, json>> filter_sort(const json& data,
-                                        function<bool(const string&, const json&)> filter, 
-                                        function<bool(const pair<string, json>&, const pair<string, json>&)> sorter
-) {
-    vector<pair<string, json>> results;
+struct Filter_Sorter {
+    using FilterType =  function<bool(const string&, const json&)>;
+    using SorterType = function<bool(const pair<string, json>&, const pair<string, json>&)>;
 
-    for (auto& [name, snip] : data.items()) {
-        if (filter(name, snip)) {
-            results.push_back({name, snip});
+    FilterType filter;
+    SorterType sorter;
+
+    vector<pair<string, json>> make_newdata(const json& data) {
+        vector<pair<string, json>> results;
+
+        for (auto& [name, snip] : data.items()) {
+            if (filter(name, snip)) {
+                results.push_back({name, snip});
+            }
         }
+
+        sort(results.begin(), results.end(), sorter);
+
+        return results;
     }
 
-    sort(results.begin(), results.end(), sorter);
+    void print_data(const json& data, const bool& full) {
+        vector<pair<string, json>> new_data = make_newdata(data);
+        for (auto& [name, snip] : new_data) {
+        cout << "name: " << name << "\n";
+        cout << "prefix: " << snip["prefix"].get<string>() << "\n";
+        if (full) {
+            if (snip.contains("description") && !snip["description"].empty()) {
+                cout << "description: " << snip["description"].get<string>() << "\n";
+            } 
 
-    return results;
-}
+            if (snip.contains("tags") && !snip["tags"].empty()) {
+                cout << "tags: ";
+                for (auto& t : snip["tags"].get<vector<string>>()) cout << t << " ";
+                cout << "\n";
+            }
+        }
+        cout << "----------------------------\n";
+    }
+    }
+};
