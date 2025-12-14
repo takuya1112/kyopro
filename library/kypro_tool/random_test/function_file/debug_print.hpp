d05a4d1ef74a9e2f9885caf5fc5964f5168b0fb0
Takuya Muraoka <muraoka1112@icloud.com>
2025-12-14 16:27:59 +0900

    change

--- library/kypro_tool/random_test/function_file/debug_print.hpp
+++ library/kypro_tool/random_test/function_file/debug_print.hpp
@@ -3,54 +3,68 @@
 using namespace std;
 
 template<class T> 
-concept OstreamPrintable = requires(ostream &s, const T& x) {
+concept Printable = requires(ostream& s, const T& x) {
     { s << x } -> same_as<ostream&>;
 };
 
 // tuple
-template <class... T> ostream& operator << (ostream &s, const tuple<T...>& P) 
-{ apply([&](const auto&&... args) { int i = 0; (( s << (i ++ ? " " : "\n") << args), ...);}, P); return s; }
+template <class... T> 
+ostream& operator << (ostream& s, const tuple<T...>& P) { 
+    apply([&](const auto&&... args) { 
+        int i = 0; 
+        (( s << (i ++ ? " " : "\n") << args), ...);
+    }, P); 
+    return s; 
+}
 
 // pair
-template<class T1, class T2> ostream& operator << (ostream &s, const pair<T1,T2>& P)
+template<class T1, class T2> ostream& operator << (ostream& s, const pair<T1,T2>& P)
 { return s << '<' << P.first << ", " << P.second << '>'; }
 
 // array
-template<class T> ostream& operator << (ostream &s, const array<T, 2>& P)
+template<class T> ostream& operator << (ostream& s, const array<T, 2>& P)
 { return s << '<' << P[0] << ", " << P[1] << '>'; }
-template<class T> ostream& operator << (ostream &s, const array<T, 3>& P)
+template<class T> ostream& operator << (ostream& s, const array<T, 3>& P)
 { return s << '<' << P[0] << ", " << P[1] << ", " << P[2] << '>'; }
-template<class T> ostream& operator << (ostream &s, const array<T, 4>& P)
+template<class T> ostream& operator << (ostream& s, const array<T, 4>& P)
 { return s << '<' << P[0] << ", " << P[1] << ", " << P[2] << ", " << P[3] << '>'; }
 
 // vector
-template<class T> ostream& operator << (ostream &s, const vector<T>& P)
+template<class T> ostream& operator << (ostream& s, const vector<T>& P)
 { s << "\n"; for (int i = 0; i < P.size(); ++i) { if (i > 0) { s << " "; } s << P[i]; } return s; }
-template<class T> ostream& operator << (ostream &s, const vector<vector<T> >& P)
+template<class T> ostream& operator << (ostream& s, const vector<vector<T> >& P)
 { for (int i = 0; i < P.size(); ++i) { s << P[i]; } return s; }
 
 // deque
-template<class T> ostream& operator << (ostream &s, const deque<T>& P)
+template<class T> ostream& operator << (ostream& s, const deque<T>& P)
 { for (int i = 0; i < P.size(); ++i) { if (i > 0) { s << " "; } s << P[i]; } return s; }
 
 // set
-template<class T> ostream& operator << (ostream &s, const set<T>& P)
+template<class T> ostream& operator << (ostream& s, const set<T>& P)
 { for (auto it : P) { s << "<" << it << "> "; } return s; }
-template<class T> ostream& operator << (ostream &s, const multiset<T>& P)
+template<class T> ostream& operator << (ostream& s, const multiset<T>& P)
 { for (auto it : P) { s << "<" << it << "> "; } return s; }
-template<class T> ostream& operator << (ostream &s, const unordered_set<T>& P)
+template<class T> ostream& operator << (ostream& s, const unordered_set<T>& P)
 { for (auto it : P) { s << "<" << it << "> "; } return s; }
 
 // map
-template<class T1, class T2> ostream& operator << (ostream &s, const map<T1,T2>& P)
+template<class T1, class T2> ostream& operator << (ostream& s, const map<T1,T2>& P)
 { for (auto it : P) { s << "<" << it.first << "->" << it.second << "> "; } return s; }
-template<class T1, class T2> ostream& operator << (ostream &s, const unordered_map<T1,T2>& P)
+template<class T1, class T2> ostream& operator << (ostream& s, const unordered_map<T1,T2>& P)
 { for (auto it : P) { s << "<" << it.first << "->" << it.second << "> "; } return s; }
 
 // tie()
 template<class T>
-concept HasTie = requires(ostream &s, const T& t) 
-{ { t.tie() }; { s << t.tie() } -> same_as<ostream&>; };
+requires requires(const T& t) { t.tie(); }
+ostream& operator << (ostream& s, const T& t) {
+    return s << t.tie();
+}
 
-template <HasTie T> ostream& operator << (ostream &s, const T& t) 
-{ return s << t.tie(); }
+// concept HasPrintableTie = requires(ostream& s, const T& t) 
+// { { t.tie() }; { s << t.tie() }-> same_as<ostream&>; };
+
+
+// template <class T> 
+// requires HasPrintableTie<T>
+// ostream& operator << (ostream& s, const T& t) 
+// { return s << t.tie(); }
