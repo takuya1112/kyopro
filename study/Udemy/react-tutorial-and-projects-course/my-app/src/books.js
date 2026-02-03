bb38a90b6de9826312698feab6e2e6eacaaaa26f
Takuya Muraoka <muraoka1112@icloud.com>
2026-02-03 17:16:01 +0900

    change

---
+++ study/Udemy/react-tutorial-and-projects-course/my-app/src/books.js
@@ -0,0 +1,39 @@
+import book1 from "./images/book1.jpg";
+import book2 from "./images/book2.jpg";
+import book3 from "./images/book3.jpg";
+import book4 from "./images/book4.jpg";
+import book5 from "./images/book5.jpg";
+
+export const books = [
+  {
+    author: "草薙龍瞬",
+    title: "Practice Without Reaction",
+    img: book1,
+    id: 1,
+  },
+
+  {
+    author: "山崎 圭一",
+    title: "一度読んだら絶対に忘れない世界史の教科書",
+    img: book2,
+    id: 2,
+  },
+  {
+    author: "すずき ちか",
+    title: "ゆとり育児ちょっといい話 ゆとりをもつほど、愛が深まる",
+    img: book3,
+    id: 3,
+  },
+  {
+    author: "辻村 深月",
+    title: "傲慢と善良",
+    img: book4,
+    id: 4,
+  },
+  {
+    author: "水野 敬也",
+    title: "夢をかなえるゾウ1",
+    img: book5,
+    id: 5,
+  },
+];
